cff0d3d9805d34e3285358e7436ad02eaa4a133b
Rohit Kisto <rohit_rtk@hotmail.com>
2023-07-11 16:20:22 -0400

    Prepping for next migration

--- src/sections/RAbout.tsx
+++ src/sections/RAbout.tsx
@@ -4,13 +4,8 @@ import Navbar from "@components/React/RNavbar";
 import LineAround from "@components/React/LineAround";
 
 import Test from "@assets/banner-1.jpg";
-import ProjectMenu from "@components/React/UI/ProjectMenu";
 
-interface Props {
-  currentUrl?: string;
-}
-
-export default function Projects({ currentUrl }: Props) {
+export default function About() {
   return (
     <div className="w-full h-full flex flex-col items-center justify-start">
       <Navbar />
@@ -20,10 +15,19 @@ export default function Projects({ currentUrl }: Props) {
           <Typography
             variant="h1"
             className="p-0 text-center text-dt-light-blue relative mx-[1vh] my-auto font-body before:content-none before:min-h-[2px] before:min-w-[50vw] before:bg-black before:absolute before:top-1/2">
-            Projects
+            About
           </Typography>
         </LineAround>
-        <ProjectMenu />
+        <section className="flex justify-center items-center h-full">
+          <Typography className="font-body" variant="h4">
+            Hi! I'm Rohit, and I'm a Software Developer. I'm also a graduate of
+            the University of Toronto Mississauga. I specialize in building
+            full-stack web applications primarily using React, TypeScript, and
+            Tailwind. I also have interests in Application Development, API
+            Development, Game Development, and Graphics Programming. In my free
+            time, I enjoy weight lifting, playing guitar, and reading.
+          </Typography>
+        </section>
       </div>
     </div>
   );
