b9ed5438b7ed94a6cdeb4006ce413ace640d3617
Rohit Kisto <rohit_rtk@hotmail.com>
2023-07-16 01:36:23 -0400

    Forgot to add export option

--- next.config.js
+++ next.config.js
@@ -1,16 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'firebasestorage.googleapis.com',
-      }
-    ],
     dangerouslyAllowSVG: true,
     contentDispositionType: 'attachment',
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-  }
+  },
+  output: "export"
 }
 
 module.exports = nextConfig
